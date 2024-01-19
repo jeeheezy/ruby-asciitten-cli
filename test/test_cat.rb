bfbdbab83ed74da95a49cb5013577ba71b24fbf8
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-01-19 20:59:29 +0000

    Added in unit test for text bubble resizing for text greater than 100 characters. Removed test_prompters.rb as after talking with Dustin, agreed that testing this would be much more work and difficulty than is worth, since there's so many conditions and some nesting of conditionals as well.

--- test/test_cat.rb
+++ test/test_cat.rb
@@ -16,4 +16,22 @@ class TestCat < Minitest::Test
 じしˍ,)ノ\n
 ", cat.ascii_cat, "ascii_cat method failed"
   end
+
+  def test_longer_text_cat
+    cat = Asciitten.new
+    cat.text = "This is a longer string that should evaluate properly and resize the text box so that the I do not get a result that overflows the text box. Otherwise, the asciitten will stay the same but this is the basis of how it is used in the prompter."
+    assert_equal "
++----------------------------------------------------------------------------------------------------+
+| This is a longer string that should evaluate properly and resize the text box so that the I do not |
+| get a result that overflows the text box. Otherwise, the asciitten will stay the same but this is  |
+|                            the basis of how it is used in the prompter.                            |
++----------------------------------------------------------------------------------------------------+
+    /
+   / 
+ ╱|、   
+(˚ˎ 。7 
+|、˜〵        
+じしˍ,)ノ\n
+", cat.ascii_cat, "longer text ascii_cat method failed"
+  end
 end
